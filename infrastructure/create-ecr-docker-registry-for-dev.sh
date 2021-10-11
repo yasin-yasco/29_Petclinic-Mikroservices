76504712d08bfbecb411060e5d483511c3631655
yasin-yasco <ozluyasin20@gmail.com>
2021-10-11 22:56:37 +0000

    added script for creating ECR registry for dev

--- infrastructure/create-ecr-docker-registry-for-dev.sh
+++ infrastructure/create-ecr-docker-registry-for-dev.sh
@@ -1,9 +1,9 @@
 PATH="$PATH:/usr/local/bin"
-APP_REPO_NAME="yasco-repo/petclinic-app-dev" # Repo ismi kucuk olmak zorun da, yoksa hata alirsin
+APP_REPO_NAME="yasco-repo/petclinic-app-dev"
 AWS_REGION="eu-west-2"
 
 aws ecr create-repository \
   --repository-name ${APP_REPO_NAME} \
-  --image-scanning-configuration scanOnPush=false \ 
-  --image-tag-mutability MUTABLE \  # uzerine yazilabilir diyoruz
+  --image-scanning-configuration scanOnPush=false \
+  --image-tag-mutability MUTABLE \
   --region ${AWS_REGION}
